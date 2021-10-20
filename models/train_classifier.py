30cd235bd290c403d7bd17d4b3cebf880387ebd6
BOTH <jerome.both@gmail.com>
2021-10-20 23:00:20 +0200

    Initial comit

---
+++ models/train_classifier.py
@@ -0,0 +1,127 @@
+import sys
+import nltk
+nltk.download(['punkt', 'wordnet','stopwords'])
+
+
+# import libraries
+import pandas as pd
+import numpy as np
+import re
+import pickle
+from sqlalchemy import create_engine
+from sklearn.datasets import make_multilabel_classification
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk import pos_tag
+import sklearn
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+
+nltk.download('averaged_perceptron_tagger')
+
+def load_data(database_filepath):
+    # load data from database
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('DisasterData', 'sqlite:///'+database_filepath) 
+    X = df.message.values
+    Y = df.loc[:, 'related':'direct_report'].values
+    category_names=df.columns[4:].tolist()
+    return X,Y,category_names
+
+def tokenize(text):
+    stop_words = stopwords.words("english")# Define stop words 
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())# Normalize text   
+    tokens = word_tokenize(text) # Tokenize
+    lemmatizer = WordNetLemmatizer()
+    
+    # lemmatize and remove stop words
+    clean_tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+    clean_tokens_filtered = [wt for (wt, tag) in pos_tag(clean_tokens) if tag in ['VB','VBP','VBG','VBZ','NN','NNS']]
+    return clean_tokens_filtered
+
+def build_model():
+    #build machine learning pipeline
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('classifier' , MultiOutputClassifier(RandomForestClassifier(), n_jobs=-1)),
+    ])
+    
+    #The following code was ran once in the jupyter notebook to determine the best parameters
+        #X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
+        #parameters ={
+        #    'vect__ngram_range': ((1, 1), (1, 2)),
+        #    'vect__max_df': (0.5, 0.75, 1.0),
+        #    'vect__max_features': (None, 5000, 10000),
+        #    'tfidf__use_idf': (True, False),
+        #    'classifier__estimator__n_estimators': [50, 100, 200],
+        #    'classifier__estimator__min_samples_split': [2, 3, 4],
+        #}
+        #cv = GridSearchCV(pipeline, param_grid=parameters)
+        #cv.fit(X_train , Y_train)
+    #outcome of the grid search gives following parameters;
+    #However, the n_estimators parameter which applies to the random forest classifier has been reduced 
+    #to limit the size of the pickle file.
+    best_parameters={'classifier__estimator__min_samples_split':2,
+                'classifier__estimator__n_estimators':10,
+                'tfidf__use_idf': True,
+                'vect__max_df': 1.0,
+                'vect__max_features': 5000,
+                'vect__ngram_range': (1,2)}
+    
+    #apply best parameters to pipeline.
+    pipeline.set_params(**best_parameters)
+
+    return pipeline
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    #predict model
+    Y_pred= model.predict(X_test)
+    
+    #print scores
+    for i in range(Y_test.shape[1]):
+        print(category_names[i])
+        print(classification_report(Y_test[:, i],Y_pred[:, i]))
+
+def save_model(model, model_filepath):
+    with open(model_filepath, 'wb') as f:
+        pickle.dump(model, f)
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
